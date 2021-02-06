d4512a3b3a7156fb5560f38a22578bc7f0f91030
Степан <stepantishhen@mail.ru>
2021-02-06 10:48:02 +0300

    second

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
+data = []
+
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
-        self.con = sqlite3.connect("coffee.sqlite")
+        self.con = sqlite3.connect("../espresso_yandexlyceum/coffee.sqlite")
         self.pushButton.clicked.connect(self.update_result)
-        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.openform)
         self.modified = {}
         self.titles = None
 
     def update_result(self):
         cur = self.con.cursor()
         # Получили результат запроса, который ввели в текстовое поле
-        result = cur.execute("SELECT * FROM films").fetchall()
+        result = cur.execute("SELECT * FROM info").fetchall()
         # Заполнили размеры таблицы
         self.tableWidget.setRowCount(len(result))
         self.tableWidget.setColumnCount(len(result[0]))
@@ -29,6 +32,27 @@ class MyWidget(QMainWindow):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
         self.modified = {}
+
+    def openform(self):
+        self.form = MyWidget2()
+        self.form.show()
+
+
+class MyWidget2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.pushButton.clicked.connect(self.get_text)
+
+    def get_text(self):
+        global data
+        data.clear()
+        data = [self.lineEdit_4.text(),
+               self.lineEdit_2.text(),
+               self.lineEdit_3.text(),
+               self.lineEdit_5.text(),
+               self.lineEdit_6.text()]
+        self.hide()
 
 
 if __name__ == '__main__':
